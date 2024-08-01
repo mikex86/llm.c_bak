5c456cb0da81c2d9ae5217e22fc869908439292d
Michael K <michael.keiblinger@gmail.com>
2024-08-01 23:18:48 +0200

    improve wrapper launcher

--- wrapper.sh
+++ wrapper.sh
@@ -1,1 +1,9 @@
+# if venv doesn't exist, create it
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+fi
+
+
 sudo venv/bin/python wrapper.py "$@"
